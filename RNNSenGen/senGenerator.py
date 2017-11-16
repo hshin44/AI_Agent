460b5d8e65f56d5ee2c1166757750b4a7e401bd0
SNUMACCORE <snumaccore@CORE-Main.local>
2017-11-16 16:00:31 +0900

    Creation of RnnSenGen

---
+++ RNNSenGen/senGenerator.py
@@ -0,0 +1,83 @@
+import theano as theano
+from rnn_theano import RNNTheano
+import csv
+import itertools
+import nltk
+import numpy as np
+#from train_theano import train_with_sgd
+from utils import load_model_parameters_theano, save_model_parameters_theano
+
+unknown_token = "UNKNOWN_TOKEN"
+sentence_start_token = "SENTENCE_START"
+sentence_end_token = "SENTENCE_END"
+
+vocabulary_size = 8000
+model = RNNTheano(vocabulary_size, hidden_dim=50)
+load_model_parameters_theano('./data/trained-model-theano.npz', model)
+
+print "Reading CSV file..."
+with open('data/reddit-comments-2015-08.csv', 'rb') as f:
+    reader = csv.reader(f, skipinitialspace=True)
+    reader.next()
+    # Split full comments into sentences
+    sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
+    # Append SENTENCE_START and SENTENCE_END
+    sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
+print "Parsed %d sentences." % (len(sentences))
+
+# Tokenize the sentences into words
+tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+
+# Count the word frequencies
+word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+print "Found %d unique words tokens." % len(word_freq.items())
+
+# Get the most common words and build index_to_word and word_to_index vectors
+vocab = word_freq.most_common(vocabulary_size-1)
+index_to_word = [x[0] for x in vocab]
+index_to_word.append(unknown_token)
+word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
+
+print "Using vocabulary size %d." % vocabulary_size
+print "The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1])
+
+def forward_propagation(self, x):
+    # The total number of time steps
+    T = len(x)
+    # During forward propagation we save all hidden states in s because need them later.
+    # We add one additional element for the initial hidden, which we set to 0
+    s = np.zeros((T + 1, self.hidden_dim))
+    s[-1] = np.zeros(self.hidden_dim)
+    # The outputs at each time step. Again, we save them for later.
+    o = np.zeros((T, self.word_dim))
+    # For each time step...
+    for t in np.arange(T):
+        # Note that we are indxing U by x[t]. This is the same as multiplying U with a one-hot vector.
+        s[t] = np.tanh(self.U[:,x[t]] + self.W.dot(s[t-1]))
+        o[t] = softmax(self.V.dot(s[t]))
+    return [o, s]
+
+def generate_sentence(model):
+    # We start the sentence with the start token
+    new_sentence = [word_to_index[sentence_start_token]]
+    # Repeat until we get an end token
+    while not new_sentence[-1] == word_to_index[sentence_end_token]:
+        next_word_probs = model.forward_propagation(new_sentence)
+        sampled_word = word_to_index[unknown_token]
+        # We don't want to sample unknown words
+        while sampled_word == word_to_index[unknown_token]:
+            samples = np.random.multinomial(1, next_word_probs[-1])
+            sampled_word = np.argmax(samples)
+        new_sentence.append(sampled_word)
+    sentence_str = [index_to_word[x] for x in new_sentence[1:-1]]
+    return sentence_str
+
+num_sentences = 10
+senten_min_length = 7
+
+for i in range(num_sentences):
+    sent = []
+    # We want long sentences, not sentences with one or two words
+    while len(sent) < senten_min_length:
+        sent = generate_sentence(model)
+    print " ".join(sent)
